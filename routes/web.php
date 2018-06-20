668c8c3efad645561fe0c279391c6ff6f17cf18c
EC2 Default User <ec2-user@ip-172-31-71-103.ec2.internal>
2018-06-20 19:29:08 +0900

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -47,5 +47,14 @@ Lesson12 8.2 ログイン認証付きのルーティング追加
 */
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+
+    /* Lesson12 10.2 フォロー機能*/
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]); /*Lesson12 9.2*/
 });
